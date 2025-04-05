a2907622a025106f3de91660ac30eda2a0a238bb
Riaa <riya210patel@gmail.com>
2025-04-05 13:25:52 +0530

    Update AuthRoutes.js

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import { login, register,getUser,forgetPassword,resetPassword } from '../controllers/AuthController.js';
 
@@ -9,17 +8,5 @@ AuthRouter.post('/api/login', login);
 AuthRouter.post('/api/forget-password',forgetPassword);
 AuthRouter.post('/api/reset-password',resetPassword);
 AuthRouter.get('/api/user',getUser);
-=======
-import express from "express";
-import { login, register } from "../controllers/AuthController.js";
-
-const AuthRouter = express.Router();
-
-AuthRouter.post("/api/register", register);
-AuthRouter.post("/api/login", login);
->>>>>>> main
-
-console.log("AuthRoutes.js loaded successfully.");
-
 
 export { AuthRouter };
