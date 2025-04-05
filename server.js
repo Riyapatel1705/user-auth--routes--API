ae010e4f2cdcaae00e2ed489a060dd3d56f5d50a
Riaa <riya210patel@gmail.com>
2025-04-05 13:14:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,13 +29,7 @@ app.use((req, res, next) => {
   }
 })();
 
-<<<<<<< HEAD
-
-
-const PORT = process.env.PORT || 7000;
-=======
-const PORT = process.env.PORT || 6000;
->>>>>>> main
+const PORT = process.env.PORT || 7000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
